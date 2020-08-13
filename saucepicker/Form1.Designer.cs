2beebf7e938d77d04c3177c2c6b7293761883864
totalatomic <63284260+totalatomic@users.noreply.github.com>
2020-08-13 18:20:15 +0200

    all the code shit
    yes

---
+++ saucepicker/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace saucepicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saucebox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "press the button to start";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(211, 53);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saucebox1
+            // 
+            this.saucebox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.saucebox1.Location = new System.Drawing.Point(211, 157);
+            this.saucebox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.saucebox1.Name = "saucebox1";
+            this.saucebox1.Size = new System.Drawing.Size(145, 20);
+            this.saucebox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(252, 133);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "your sauce:";
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(359, 157);
+            this.searchBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(94, 20);
+            this.searchBtn.TabIndex = 4;
+            this.searchBtn.Text = "search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(356, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "*warning* opens link in normal tab";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 365);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.saucebox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox saucebox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ saucepicker/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saucepicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            generator();
+            
+        }
+        //all of the posible numbers
+        string[] sauce = { "193031", "000136", "123911"," 266576", "5112","214070" , "125906",
+                "234638", "274290","265918" ,"136489" ,"283975" ,"212180","244327" ,"271282" ,"190997" , "248589", "271282", "60476", "275327", "131598",
+                "285679", "070925", "288610", "194235", "274917", "097479", "219059", "270925", "229779", "145066", "205079", "290038", "277677", "278154", "223083",
+                "293914","102597" ,"294490" , "294453","168333" ,"223156" ,"266987" ,"294298" ,"146170" ,"267270" ,"274329" ,"110150" ,"255662" ,"267975" ,
+                 "294505","230048", "272241" , "226416" , "266984", "290886","264032", "289060","219610" ,"243719" ,"263806","236195",  "275232","214877",
+                "281415","199449","233736","283818","258693","259877","162059","219624","259029"};
+        //the generation of the random number
+        public int generator()
+        {
+            //make a random
+            Random rnd = new Random();
+            //gets the random number that cant be bigger than the number of options in the array
+            int numbers = rnd.Next(0, sauce.Length);
+            //returns the random number
+            saucebox1.Text = sauce[numbers];
+            return numbers;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generator();
+        }
+
+        public void searchBtn_Click(object sender, EventArgs e)
+        {
+            ///NTU stands for numbers to use
+            string NTU = saucebox1.Text;
+            if (NTU.Length > 6 || NTU.Length <6)
+            {
+                saucebox1.Clear();
+                MessageBox.Show("that is incorect please fill in the 6 digets");
+            }
+            else 
+            {
+                if (long.TryParse(saucebox1.Text, out long isparsable))
+                {
+                    System.Diagnostics.Process.Start($"https://nhentai.net/g/{NTU}");
+                }
+                else
+                {
+                    saucebox1.Clear();
+                    MessageBox.Show("Only Numbers Allowed");
+                    
+                }
+
+            }
+        }
+    }
+}
